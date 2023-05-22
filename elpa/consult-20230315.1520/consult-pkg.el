9af00a7a8039245d57e2133eedabecb05ebb4015
Шапошников Евгений <shaposhnikovea@lad24.ru>
2023-05-22 14:09:20 +0300

    Create

---
+++ elpa/consult-20230315.1520/consult-pkg.el
@@ -0,0 +1,11 @@
+(define-package "consult" "20230315.1520" "Consulting completing-read"
+  '((emacs "27.1")
+    (compat "29.1.4.0"))
+  :commit "3052c57969ddd7dc23e325a2703b5f14d33d1133" :authors
+  '(("Daniel Mendler and Consult contributors"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :url "https://github.com/minad/consult")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
